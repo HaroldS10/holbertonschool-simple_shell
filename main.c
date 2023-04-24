a97dc77d480c65e5face3e93714d1677d4187372
HaroldS10 <haroldsanty13@gmail.com>
2023-04-24 09:44:34 -0700

    principal fucntion to shell

---
+++ main.c
@@ -0,0 +1,46 @@
+#include "shell.h"
+
+/**
+ *main - entry point for the shell program
+ *Return: 0 on success
+ */
+
+int main(void)
+{
+	char *buffer = malloc(500);
+	size_t bufsize = 0;
+	ssize_t nread;
+	char **args = NULL;
+	char **argv = NULL;
+	
+
+	while (1)
+	{
+
+		nread = getline(&buffer, &bufsize, stdin);
+		if (nread == -1)
+		{
+			if (feof(stdin))
+			{
+				break;
+			}
+			else
+			{
+				perror("readline");
+				exit(EXIT_FAILURE);
+			}
+		}
+
+		if (*buffer == '\0')
+			continue;
+
+		args = parse_line(buffer);
+
+		execute(argv);
+		free(args);
+	}
+
+	free(buffer);
+	return (0);
+
+}
